e342df26f83a6913ffc857049651df50319e2020
sui <suimakki8@gmail.com>
2024-09-02 20:38:35 +0530

    Structure 3

--- com.inn.cafe/src/main/java/com/inn/cafe/POJO/User.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/POJO/User.java
@@ -1,7 +1,6 @@
 package com.inn.cafe.POJO;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
@@ -14,4 +13,30 @@ import java.io.Serializable;
 @DynamicUpdate
 @Table(name = "user")
 public class User implements Serializable {
+    public static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "contactNumber")
+    private String ContactNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "role")
+    private String role;
+
+
 }
